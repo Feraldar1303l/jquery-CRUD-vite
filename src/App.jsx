b9412a352bbe0f99172ec000c77e0fc2fcb8a5da
Fernando <bocanegrafer04gmail.com>
2024-04-08 14:17:11 -0600

    First comment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProductForm from "./components/ProductForm";
+import Products from "./components/Products";
+
+function MyApp() {
+  return(
+    <>
+    <ProductForm/>
+    <Products/>
+    </>
+  )
+}
+
+export default MyApp;
